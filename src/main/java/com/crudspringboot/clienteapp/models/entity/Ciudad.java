64d1a6d3beb477c1189f676e309b421cdab3bffd
Jaime <jaimea.cabreraa@gmail.com>
2020-06-03 12:37:55 -0500

    Creacion de clases

--- src/main/java/com/crudspringboot/clienteapp/models/entity/Ciudad.java
+++ src/main/java/com/crudspringboot/clienteapp/models/entity/Ciudad.java
@@ -1,5 +1,45 @@
 package com.crudspringboot.clienteapp.models.entity;
 
-public class Ciudad {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Ciudad implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idCiudad;
+	private String ciudad;
+
+	public Long getIdCiudad() {
+		return idCiudad;
+	}
+
+	public void setIdCiudad(Long idCiudad) {
+		this.idCiudad = idCiudad;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	@Override
+	public String toString() {
+		return "Ciudad [idCiudad=" + idCiudad + ", ciudad=" + ciudad + "]";
+	}
 
 }
--- src/main/java/com/crudspringboot/clienteapp/models/entity/Cliente.java
+++ src/main/java/com/crudspringboot/clienteapp/models/entity/Cliente.java
@@ -1,15 +1,23 @@
 package com.crudspringboot.clienteapp.models.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "clientes")
-public class Cliente {
+public class Cliente implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idCliente;
@@ -18,6 +26,78 @@ public class Cliente {
 	private String telefono;
 	private String email;
 	private String ciudad;
+	
+	@ManyToOne
+	@JoinColumn(name="id_ciudad")
 	private Ciudad idCiudad;
+
+	public Long getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Long idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public Ciudad getIdCiudad() {
+		return idCiudad;
+	}
+
+	public void setIdCiudad(Ciudad idCiudad) {
+		this.idCiudad = idCiudad;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [idCliente=" + idCliente + ", nombres=" + nombres + ", apellidos=" + apellidos + ", telefono="
+				+ telefono + ", email=" + email + ", ciudad=" + ciudad + ", idCiudad=" + idCiudad + "]";
+	}
+	
+	
+	
+	
+	
+
+
 
 }
