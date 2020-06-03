e4a1a72a36f253f44d2bdc21dcca01f4b3b916cd
Jaime <jaimea.cabreraa@gmail.com>
2020-06-03 17:36:19 -0500

    Listar Cliente 2

--- src/main/java/com/crudspringboot/clienteapp/models/entity/Cliente.java
+++ src/main/java/com/crudspringboot/clienteapp/models/entity/Cliente.java
@@ -24,6 +24,7 @@ public class Cliente implements Serializable {
 	private String nombres;
 	private String apellidos;
 	private String telefono;
+	private String direccion;
 	private String email;
 	
 	@ManyToOne
@@ -61,6 +62,15 @@ public class Cliente implements Serializable {
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
 	}
+	
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
 
 	public String getEmail() {
 		return email;
